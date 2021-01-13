0581cdca440186bfce5b52d0bae9547d941f3525
Brandon Collins <bcollin3@uccs.edu>
2021-01-12 20:38:08 -0700

    Finalized Code

--- src/ChromeBat.py
+++ src/ChromeBat.py
@@ -18,22 +18,8 @@ from itertools import repeat
 comment_re=re.compile("#.*")
 numeric_re=re.compile('\d+(\.\d+)?')
 
-#accepts a canidate solution(xyz coordinates of all loci)
+#accepts a canidate solution(xyz coordinates of all loci) can be have shape (3n,) or (n,3)
 #returns the distance matrix between all of said loci
-def sol2dist(sol):
-	if len(np.shape(sol))==1:
-		xyz_col=np.array(np.split(sol,len(sol)/3))
-	else:
-		xyz_col=sol
-	
-	matrix=np.zeros((len(xyz_col),len(xyz_col)))
-	for i in range(len(xyz_col)):
-		x_i,y_i,z_i=xyz_col[i]
-		for j in range(len(xyz_col)):
-			x_j,y_j,z_j=xyz_col[j]
-			matrix[i,j]=((x_i-x_j)**2+(y_i-y_j)**2+(z_i-z_j)**2)**.5
-	return matrix
-
 def sol2distVec(sol):
     #Ensuring xyz_col to have shape (n,3)
     if len(np.shape(sol))==1:
@@ -59,20 +45,20 @@ def contactToDistance(contact,alpha=.5):
 
 
 
-#this is the optimization algorithm
-#it is based on the paper and https://github.com/buma/BatAlgorithm
-#call it by using fly()
+#this is the optimization is performed
+#initilize a bat object and then call fly()
+#parameters are described on github
 class Bat:
 	params=set(["matrix","structs","alpha","func","num_bats","upper_bound","lower_bound","min_freq","max_freq","volume","generations","pulse","perturbation"])
 	def __init__(self,matrix,num_bats=20,lower_bound=0,upper_bound=3,min_freq=0,max_freq=2,volume=0.5,generations=100,pulse=0.5,perturbation=0.01):
-		self.lower=lower_bound
-		self.upper=upper_bound
+		self.lower=lower_bound 
+		self.upper=upper_bound 
 		self.freq_min=min_freq
 		self.freq_max=max_freq
 		self.D=len(matrix)*3
 		self.matrix=matrix
-		self.generations=int(generations)
-		self.perturbation=perturbation
+		self.generations=int(generations) #how many iterations of the algorithm
+		self.perturbation=perturbation # this dictates the size of a bats random walk after pulsing
 		# self.loss=func
 		self.loudness=volume
 		self.num_bats=int(num_bats)
@@ -82,7 +68,7 @@ class Bat:
 		self.round=lambda x:0 if x>=self.pulse else 1
 		self.freq=np.zeros(self.num_bats)
 		self.sols=self.lower+(self.upper-self.lower)*np.random.rand(self.num_bats,self.D)
-		self.velo=np.zeros((self.num_bats,self.D))
+		self.velo=np.zeros((self.num_bats,self.D)) #bats velocity vector with shape (num_bats,D), where D is the number of dimensions 
 		self.fitness=np.apply_along_axis(self.loss,1,self.sols)
 		self.evalBats()
 
@@ -95,91 +81,40 @@ class Bat:
 		loss_matrix=key-sum_matrix
 		loss_matrix=np.nan_to_num(loss_matrix,copy=True,posinf=0)
 		return np.sum(np.absolute(np.tril(loss_matrix)))
-	# def loss(self,sol):
-	# 	key=self.matrix
-	# 	sol=np.array(sol)
-	# 	xyz_col=np.array(np.split(sol,len(sol)/3))
-	# 	sum_matrix=np.zeros(len(xyz_col))
-	# 	for i in [1,2,3]:
-	# 		b=np.tile(xyz_col[:,i-1:i].T,(len(xyz_col),1))
-	# 		c=np.tile(xyz_col[:,i-1:i],(1,len(xyz_col)))
-	# 		d=np.tril(b-c)
-	# 		sum_matrix=sum_matrix+(d*d)
-	# 	sum_matrix=sum_matrix **0.5
-	# 	loss_matrix=key-sum_matrix
-	# 	loss_matrix=np.nan_to_num(loss_matrix,copy=True,posinf=0)
-	# 	return np.sum(np.absolute(np.tril(loss_matrix)))
+	#this method assumes self.fitness corrospondes to self.sol
+	#sets self.best_sol,self.best_fitness accordingly
 	def evalBats(self):
 		best_bat=np.argmin(self.fitness)
 		if self.fitness[best_bat]<=self.best_fit:
 			self.best_sol=self.sols[best_bat]
 			self.best_fit=self.fitness[best_bat]
+	#this is the actual bat algorithm
+	#vectorized implementation has some redunant computation, but almost all computation time is spent in oracle calls
 	def fly(self):
 		for g in range(self.generations):
+			#bat's mobility logic
 			self.freq=self.freq_min+(self.freq_max-self.freq_min)*np.random.rand(self.num_bats)
 			self.velo=self.velo+(self.sols-self.best_sol)*self.freq[:,np.newaxis]
 			self.temp_sols=self.sols+self.velo
 			self.temp_sols=np.clip(self.temp_sols,self.lower,self.upper)
 
-			#this could become individuals bats pulse rate
+			#bat's pulse logic
 			self.pulse_array=(np.array([self.pulse]*self.num_bats)>=np.random.rand(self.num_bats)).astype(int)
 			self.flip_pulse_array=(self.pulse_array+1)%2
 			self.temp_sols=(self.temp_sols*self.flip_pulse_array[:,np.newaxis])+(np.tile(self.best_sol,(self.num_bats,1))*self.pulse_array[:,np.newaxis]+self.perturbation*np.random.normal(0,1,(self.num_bats,self.D)))
 			self.temp_sols=np.clip(self.temp_sols,self.lower,self.upper)
 
+			#oracle calls
 			self.temp_fitness=np.apply_along_axis(self.loss,1,self.temp_sols)
 
-			#this could become individual bats volume
+			#accepting soln logic
 			self.accepted_sols=(self.temp_fitness<=self.fitness).astype(int)*(np.array([self.loudness]*self.num_bats)>=np.random.rand(self.num_bats)).astype(int)
 			self.declined_sols=(self.accepted_sols+1)%2
 			self.sols=(self.sols*self.declined_sols[:,np.newaxis])+(self.temp_sols*self.accepted_sols[:,np.newaxis])
 			self.fitness=(self.fitness*self.declined_sols)+(self.temp_fitness*self.accepted_sols)
 			self.evalBats()
 		return self.best_sol
-	def fly2(self):
-		self.S=np.zeros((self.num_bats,self.D))
-		for t in range(self.generations):
-			self.freq = np.array([self.freq_min]*self.num_bats) + (np.array([self.freq_max]*self.num_bats) - np.array([self.freq_min]*self.num_bats)) * np.random.rand(self.num_bats)
-			self.velo = self.velo + (self.sols - self.best_sol) * self.freq[:,np.newaxis]
-			self.S = self.sols + self.velo
-			self.S=np.clip(self.S,self.lower,self.upper)
-			for i in range(self.num_bats):
-				if np.random.random_sample() >= self.pulse:
-					for j in range(self.D):
-						self.S[i][j] = self.best_sol[j] + 0.01 * np.random.normal(0,1)
-						self.S[i][j] = max(self.lower,self.S[i][j])
-						self.S[i][j] = min(self.upper,self.S[i][j])
-				self.Fnew = self.loss(self.S[i])
-				self.rnd = np.random.random_sample()
-				if (self.Fnew <= self.fitness[i]) and (self.rnd < self.loudness):
-					for j in range(self.D):
-						self.sols[i][j] = self.S[i][j]
-					self.fitness[i] = self.Fnew
 
-				if self.Fnew <= self.best_fit:
-					for j in range(self.D):
-						self.best_sol[j] = self.S[i][j]
-					self.best_fit = self.Fnew
-		return self.best_sol
-	def PSO(self):
-		lr=0.2 #learning rate
-		#temp_sols is bats current location
-		#self.sols is a bats best known location
-		temp_sols=self.sols.copy()
-		self.velo=np.random.uniform(self.lower-self.upper,self.upper-self.lower,(self.num_bats,self.D))
-		for g in range(self.generations):
-			global_mult_m=np.random.uniform(0,1,(self.num_bats,self.D))#r_g from wiki
-			local_mult_m=np.random.uniform(0,1,(self.num_bats,self.D))#r_p from wiki
-			#we omit omega,phi_p,phi_g from wiki for now
-			self.velo=self.velo+local_mult_m*(self.sols-temp_sols)+global_mult_m*(np.tile(self.best_sol,(self.num_bats,1))-temp_sols)
-			temp_sols=temp_sols+self.velo*lr
-			temp_fitness=np.apply_along_axis(self.loss,1,temp_sols)
-			accepted_sols=(temp_fitness<self.fitness).astype(int)
-			declined_sols=(accepted_sols+1)%2
-			self.sols=(self.sols*declined_sols[:,np.newaxis])+(temp_sols*accepted_sols[:,np.newaxis])
-			self.fitness=(self.fitness*declined_sols)+(temp_fitness*accepted_sols)
-			self.evalBats()
-		return self.best_sol
 
 #accepts symmetric square matrix a
 #returns a with zero adjancent bins replaced with an average adjancency value
@@ -194,7 +129,7 @@ def adjancenyPreprocess(a):
 
 #used for making the pdb files
 #pads the input string so it is size long
-#left=True pads on the left False on the Right
+#left=True pads on the left False on the right
 def pad(value,size,left=False):
 	value=str(value)
 	space_to_add=size-len(value)
@@ -229,11 +164,11 @@ def outputPdb(input_sol,outfile=None):
 	f.write(out_string)
 	f.close()
 #this does all the command line processing
-#return (contact matrix,alpha,bat algo parameters,outfile,fly_ver)
+#return (contact matrix,alpha,bat algo parameters,outfile,structs)
 def processInput(input_fname,parameter_fname=None):
 	contact=np.genfromtxt(input_fname)
 	alpha=None
-	fly_ver="1"
+	structs=1
 	outfile=None
 	if parameter_fname is None:
 		return (contact,alpha,dict(),outfile,structs)
@@ -267,8 +202,9 @@ def processInput(input_fname,parameter_fname=None):
 
 #accepts the proposed solution and the target distance matrix
 #returns a string with PCC,SCC,RMSE
+#string=False makes it return the tuple (rmse,scc,pcc)
 def formatMetrics(sol,key,string=True):
-	distance_matrix=sol2dist(sol)
+	distance_matrix=sol2distVec(sol)
 	# key=np.nan_to_num(key,copy=True,posinf=0)
 	distance_list=[]
 	key_list=[]
@@ -288,6 +224,7 @@ def formatMetrics(sol,key,string=True):
 	return metrics
 #this writes the log file
 #accepts the metric string from formatMetrics,input file name, and an output file name
+#writes .log file accordingly
 def outputLog(metrics,alpha,input_fname,out_fname=None,bat_params=None,runtime=None,searched_alphas=None,structs=None):
 	if out_fname is None:
 		out_fname="bat.log"
@@ -305,6 +242,8 @@ def outputLog(metrics,alpha,input_fname,out_fname=None,bat_params=None,runtime=N
 	f=open(out_fname,"w")
 	f.write(outstring)
 	f.close()
+
+#simple function for multiprocessing
 def optimize(distance,params):
 	bats=Bat(distance,**params)
 	return bats.fly()
@@ -312,6 +251,9 @@ def optimize(distance,params):
 
 #driver code
 if __name__=="__main__":
+
+
+	#command line/input processing
 	start_time=time.time()
 	parser=argparse.ArgumentParser()
 	parser.add_argument("contact_matrix",help="File name of a square contact matrix")
@@ -322,10 +264,14 @@ if __name__=="__main__":
 
 	print("Running ChromeBat")
 
+	#perform alpha search if no alpha found in the parameter file
+	#sets alpha to best alpha found
 	PROC_COUNT=cpu_count()
 	searched_alphas=False
 	if alpha is None:
 		print("Performing Alpha Search...")
+
+		#multiprocessing to search the alpha values
 		searched_alphas=True
 		alphas=[0.1,0.3,0.5,0.7,0.9,1]
 		distance_m_list=[adjancenyPreprocess(contactToDistance(contact,a)) for a in alphas]
@@ -333,31 +279,44 @@ if __name__=="__main__":
 		swarms=pool.starmap(optimize, zip(distance_m_list,repeat(param_dict)))
 		pool.close()
 		pool.join()
+
+		#metrics computations and alpha selection
 		spearmans=[formatMetrics(sol,distance_m_list[i],string=False)[1] for i,sol in enumerate(swarms)]
 		best_index=np.argmax(spearmans)
 		alpha=alphas[best_index]
+		searched_sol=swarms[best_index]
 		end_alphas_time=time.time()
 		pad_len=len(str(max(alphas,key=lambda x:len(str(x)))))+7
 		alpha_scc_string="\n".join([pad(alphas[i],pad_len,left=False)+str(spearmans[i]) for i in range(len(spearmans))])
 		print("Search Results:\n"+pad("alpha",pad_len)+"SCC\n"+alpha_scc_string)
 		print(f"Best Alpha={alpha}, found in {end_alphas_time-start_time:.2f} seconds")
 
-
-	print(f"Generating {structs} structures using Alpha={alpha}")
+	#geneterates structs structures 
 	distance_m=contactToDistance(contact,alpha)
-	distance_m_prepro=adjancenyPreprocess(distance_m)
-	pool = Pool(processes=PROC_COUNT)
-	swarms=pool.starmap(optimize, zip(repeat(distance_m_prepro,structs),repeat(param_dict)))
-	pool.close()
-	pool.join()
+	if structs>0: 
+		print(f"Generating {structs} more structures using Alpha={alpha}")
+		distance_m_prepro=adjancenyPreprocess(distance_m)
+		pool = Pool(processes=PROC_COUNT)
+		swarms=pool.starmap(optimize, zip(repeat(distance_m_prepro,structs),repeat(param_dict)))
+		pool.close()
+		pool.join()
+	else: # reset swarms variable if structs happened to be 0
+		swarms=[]
+
+	#metric computations on generated structures
+	if searched_alphas: #reuse structure generated in the search portion
+		swarms.append(searched_sol)
+
 	spearmans=[formatMetrics(sol,distance_m,string=False)[1] for sol in swarms]
 	best_index=np.argmax(spearmans)
 	
-	best_sol=swarms[best_index]*10
+	best_sol=swarms[best_index]*10 # output structures tended to be very small so now they are not
 	metrics=formatMetrics(best_sol,distance_m)
 
 	final_end_time=time.time()
 	print(f"Done in {final_end_time-start_time:.2f} seconds total")
+
+	#output results
 	print("Writing best structure....")
 
 	print(metrics)
@@ -369,46 +328,6 @@ if __name__=="__main__":
 	print(f"{outfile}.log and {outfile}.pdb written!")
 
 	
-	# if alpha is None:
-	# 	for alpha in [0.1,0.3,0.5,0.7,0.9,1.0]:
-	# 		start_time=time.time()
-	# 		distance=contactToDistance(contact,alpha)
-	# 		distance_prepro=adjancenyPreprocess(distance)
-	# 		bats=Bat(distance_prepro,**param_dict)
-	# 		if fly_ver=="2":
-	# 			best_sol=bats.fly2()
-	# 		elif fly_ver=="PSO":
-	# 			best_sol=bats.PSO()
-	# 		else:
-	# 			best_sol=bats.fly()
-	# 		best_sol*=10 #resizing solotion as it is quite small
-	# 		metrics=formatMetrics(best_sol,distance)
-	# 		end_time=time.time()
-	# 		print(f"alpha={alpha}\n"+metrics)
-	# 		print(f"Execution Time: {end_time-start_time:.2f} seconds using fly_ver={fly_ver}")
-
-	# 		outfile_suffix="a"+(str(alpha)).strip(".")
-	# 		outputLog(metrics,alpha,args.contact_matrix,outfile+outfile_suffix,bat_params=param_dict,runtime=end_time-start_time)
-	# 		outputPdb(best_sol,outfile+outfile_suffix)
-	# else:
-	# 	start_time=time.time()
-	# 	distance=contactToDistance(contact,alpha)
-	# 	distance_prepro=adjancenyPreprocess(distance)
-	# 	bats=Bat(distance_prepro,**param_dict)
-	# 	if fly_ver=="2":
-	# 		best_sol=bats.fly2()
-	# 	elif fly_ver=="PSO":
-	# 		best_sol=bats.PSO()
-	# 	else:
-	# 		best_sol=bats.fly()
-	# 	best_sol*=10
-	# 	metrics=formatMetrics(best_sol,distance)
-	# 	end_time=time.time()
-	# 	print(metrics)
-	# 	print(f"Execution Time: {end_time-start_time:.2f} seconds using fly_ver={fly_ver}")
-	# 	outputLog(metrics,alpha,args.contact_matrix,outfile,bat_params=param_dict,runtime=end_time-start_time)
-	# 	outputPdb(best_sol,outfile)
-
 
 
 
